dc4699a18c4b49956f1907e3089a905a1d6ee04e
tasneembegum34 <tasneembegum34@gmail.com>
2021-06-18 21:04:41 +0530

    c

--- lawsystem/lawsystem/urls.py
+++ lawsystem/lawsystem/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
  path('',include('client.urls')),
@@ -24,4 +24,4 @@ urlpatterns = [
  path('',include('hireAdvocates.urls')),
 ]
 
-#urlpatterns =urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=staticfiles_urlpatterns()
