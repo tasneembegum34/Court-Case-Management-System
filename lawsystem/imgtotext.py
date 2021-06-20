c0246d38d32fee25c15586953d903cda693b7c8f
tasneembegum34 <tasneembegum34@gmail.com>
2021-06-20 14:31:40 +0530

    commit

--- lawsystem/imgtotext.py
+++ lawsystem/imgtotext.py
@@ -1,8 +1,11 @@
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd=r'E:\SHAREit\tasneemfiles\tesseract.exe'
 from PIL import Image
-
+import re
 img=Image.open('captcha.png')
 text=tess.image_to_string(img)
-
 print(text)
+temp=re.findall(r'\d+',text)
+res=list(map(int,temp))
+num=res[0]
+print(num)
--- lawsystem/status.py
+++ lawsystem/status.py
@@ -1,9 +1,15 @@
-import urllib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import pytesseract as tess
+import re
+from bs4 import BeautifulSoup
 from PIL import Image
-import base64
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+tess.pytesseract.tesseract_cmd=r'E:\SHAREit\tasneemfiles\tesseract.exe'
 
 driver=webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
@@ -18,8 +24,26 @@ screenshot_as_bytes=img2.screenshot_as_png
 with open('captcha.png', 'wb') as f:
     f.write(screenshot_as_bytes)
 
-with open("captcha.png", "rb") as image:
-    b64string = base64.b64encode(image.read())
+img=Image.open('captcha.png')
+text=tess.image_to_string(img)
+print(text)
+temp=re.findall(r'\d+',text)
+res=list(map(int,temp))
+print(res)
+num=res[0]
+print(num)
+driver.find_element(By.XPATH,"//input[contains(@id,'captcha')]").send_keys(str(num))
+driver.find_element(By.XPATH,"//input[contains(@id,'searchbtn')]").click()
 
-
+try:
+    myElem = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'historyform')))
+    print("Page is ready!")
+    status=driver.find_element(By.XPATH,"//form[contains(@class,'historyform')]")
+    print(status)
+    #print(status.text)
+    print("========")
+    element=driver.find_element_by_css_selector('form.historyform')
+    print(element.get_attribute('innerHtml'))
+except TimeoutException:
+    print("Loading took too much time!")
 driver.close()
